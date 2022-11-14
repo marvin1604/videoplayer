27716bda569dbcecee68496e3403ea235615b609
marvin1604 <walter.rodriguez1992@gmail.com>
2022-11-14 15:30:51 -0500

    agregando funcionalidad a la barra lateral y agregando barra para set del time del video

--- src/mediaplayer/video-data.js
+++ src/mediaplayer/video-data.js
@@ -1,7 +1,7 @@
 const videoSources = [
     {
       description:
-        "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+        "Big Buck Bunny cuenta la historia de un conejo gigante con un corazón más grande que él. Cuando un día soleado tres roedores lo acosan con rudeza, algo se rompe... ¡y el conejo ya no es más un conejito! En la típica tradición de los dibujos animados, prepara una venganza cómica para los desagradables roedores.",
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
       title: 'Big Buck Bunny',
@@ -11,7 +11,7 @@ const videoSources = [
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg',
     },
     {
-      description: 'The first Blender Open Movie from 2006',
+      description: 'La primera película abierta de Blender de 2006',
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
       title: 'Elephant Dream',
@@ -22,7 +22,7 @@ const videoSources = [
     },
     {
       description:
-        'HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.',
+        'HBO GO ahora funciona con Chromecast, la forma más fácil de disfrutar videos en línea en su televisor. Para cuando quieras instalarte en tu Trono de Hierro para ver los últimos episodios. Por $35.\nAprende a usar Chromecast con HBO GO y más en google.com/chromecast.',
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
       title: 'For Bigger Blazes',
@@ -33,7 +33,7 @@ const videoSources = [
     },
     {
       description:
-        "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
+        "Presentamos Chromecast. La forma más fácil de disfrutar de videos y música en línea en su televisor, para cuando las fugas de Batman no sean lo suficientemente grandes. Por $35. Aprende a usar Chromecast con Google Play Movies y más en google.com/chromecast.",
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
       title: 'For Bigger Escape',
@@ -44,7 +44,7 @@ const videoSources = [
     },
     {
       description:
-        'Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.',
+        'Presentamos Chromecast. La forma más fácil de disfrutar de videos y música en línea en su televisor. Por $35. Obtenga más información en google.com/chromecast.',
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
       title: 'For Bigger Fun',
@@ -55,7 +55,7 @@ const videoSources = [
     },
     {
       description:
-        'Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.',
+        'Presentamos Chromecast. La forma más fácil de disfrutar de videos y música en línea en su televisor, para los tiempos que requieren más diversión. Por $35. Aprende a usar Chromecast con YouTube y más en google.com/chromecast.',
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
       title: 'For Bigger Joyrides',
@@ -66,7 +66,7 @@ const videoSources = [
     },
     {
       description:
-        "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.",
+        "Presentamos Chromecast. La manera más fácil de disfrutar videos y música en línea en tu televisor, para cuando quieras hacer que las grandes crisis de Buster sean aún más grandes. Por $35. Aprende a usar Chromecast con Netflix y más en google.com/chromecast.",
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
       title: 'For Bigger Meltdowns',
@@ -77,7 +77,7 @@ const videoSources = [
     },
     {
       description:
-        'Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org',
+        'Sintel es un cortometraje producido de forma independiente, iniciado por la Fundación Blender como un medio para mejorar y validar aún más la suite de creación 3D de código abierto/libre Blender. Con una financiación inicial proporcionada por miles de donaciones a través de la comunidad de Internet, nuevamente ha demostrado ser un modelo de desarrollo viable tanto para la tecnología 3D abierta como para la película de animación independiente.\nEsta película de 15 minutos se realizó en el estudio del Amsterdam Blender Institute , por un equipo internacional de artistas y desarrolladores. Además de eso, varios desarrolladores, artistas y equipos de todo el mundo han logrado en línea objetivos técnicos y creativos cruciales.\nwww.sintel.org',
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
       title: 'Sintel',
@@ -88,7 +88,7 @@ const videoSources = [
     },
     {
       description:
-        'Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.',
+        'Smoking Tire lleva al nuevo Subaru Outback al punto más alto que podemos encontrar con la esperanza de que nuestro Lanzamiento del globo de apreciación del cliente obtenga algunas camisetas gratis en manos de nuestros espectadores.',
       sources:
         'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4',
       title: 'Subaru Outback On Street And Dirt',
